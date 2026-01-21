13c3eaf3cada6dfc51736d40ab718aefa9326e64
WuDiXianXin <wudixianxin@163.com>
2026-01-21 09:26:43 +0800

    update: 使用DMS-shell

--- etc-back.bash
+++ etc-back.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo rsync -av /etc/default/grub etc/default/
-sudo rsync -av /etc/modprobe.d/nvidia-pm.conf etc/modprobe.d/
-sudo rsync -av /etc/pacman.d/mirrorlist etc/pacman.d/
-sudo rsync -av /etc/pacman.conf etc/
+sudo rsync -a /etc/default/grub etc/default/
+sudo rsync -a /etc/modprobe.d/nvidia-pm.conf etc/modprobe.d/
+sudo rsync -a /etc/pacman.d/mirrorlist etc/pacman.d/
+sudo rsync -a /etc/pacman.conf etc/
